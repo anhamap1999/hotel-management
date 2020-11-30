2515f171e658171749f746b3465b7495493d331c
ducmanh1888 <nguyenducmanh1888@gmail.com>
2020-11-30 23:55:01 +0700

    them trang thay doi quy dinh + lap hoa don

--- frontend/src/components/admin/payment/index.js
+++ frontend/src/components/admin/payment/index.js
@@ -14,104 +14,102 @@ export default function PaymentScreen(){
         <div className="bill"> 
             <h1 className="text-center">Lập hóa đơn</h1>
             <div className="bill-body">
-            <div className="row">
-                <form style={{ width:"100%"}}>
-                    <div class="form-body">
-                        <div className="row">
-                            <div className="col-md-6">
-                                <div class="form-group">
-                                    <label for="name">Tên khách hàng</label>
-                                    <input type="text" class="form-control" id="CustomerName" placeholder="Vd: Nguyễn Thanh Liêm" />
+                    <form style={{ width:"100%"}}>
+                        <div class="form-body">
+                            <div className="row">
+                                <div className="col-md-6">
+                                    <div class="form-group">
+                                        <label for="name">Tên khách hàng</label>
+                                        <input type="text" class="form-control" id="CustomerName" placeholder="Vd: Nguyễn Thanh Liêm" />
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="col-md-6">
-                                <div class="form-group">
-                                    <label for="inputPassword4" >Ngày</label>
-                                    <input type="text" class="form-control" id="date" placeholder={date} disabled />
+                                <div className="col-md-6">
+                                    <div class="form-group">
+                                        <label for="inputPassword4" >Ngày</label>
+                                        <input type="text" class="form-control" id="date" placeholder={date} disabled />
+                                    </div>
                                 </div>
-                            </div>
-                        </div>     
-                        
-                        <div className="row">
-                            <div className="col-md-6">
-                                <div class="form-group">
-                                    <label for="name">Địa chỉ</label>
-                                    <input type="text" class="form-control" id="CustomerName" placeholder="Vd: Tp HCM" />
+                            </div>     
+                            
+                            <div className="row">
+                                <div className="col-md-6">
+                                    <div class="form-group">
+                                        <label for="name">Địa chỉ</label>
+                                        <input type="text" class="form-control" id="CustomerName" placeholder="Vd: Tp HCM" />
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="col-md-6">
-                                <div class="form-group">
-                                    <label for="Total" >Thành tiền</label>
-                                    <input type="text" class="form-control" id="Total"   value="2 000 000$" disabled />
+                                <div className="col-md-6">
+                                    <div class="form-group">
+                                        <label for="Total" >Thành tiền</label>
+                                        <input type="text" class="form-control" id="Total"   value="2 000 000$" disabled />
+                                    </div>
                                 </div>
+                            </div>   
+                            <div className="bill-table">
+                                <table class="table table-sm">
+                                    <thead className="text-center">
+                                        <tr>
+                                        <th scope="col">STT</th>
+                                        <th scope="col">Số Phòng</th>
+                                        <th scope="col">Số ngày thuê</th>
+                                        <th scope="col" >Đơn Giá</th>
+                                        <th scope="col">Ghi Chú</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <th scope="row" className="STT"></th>
+                                            <td>
+                                                <select class="custom-select">
+                                                    <option selected>Open this select menu</option>
+                                                    <option value="1">One</option>
+                                                    <option value="2">Two</option>
+                                                    <option value="3">Three</option>
+                                                </select>
+                                            </td>
+                                            <td><input type="text" disabled value="1" /></td>
+                                            <td ><input type="text" disabled value="200 000$" /></td>
+                                            <td><input type="text" placeholder="Ghi chú" /></td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row" className="STT"></th>
+                                            <td>
+                                                <select class="custom-select">
+                                                    <option selected>Open this select menu</option>
+                                                    <option value="1">One</option>
+                                                    <option value="2">Two</option>
+                                                    <option value="3">Three</option>
+                                                </select>
+                                            </td>
+                                            <td><input type="text" disabled value="1" /></td>
+                                            <td ><input type="text" disabled value="200 000$" /></td>
+                                            <td><input type="text" placeholder="Ghi chú" /></td>
+                                        </tr> <tr>
+                                            <th scope="row" className="STT"></th>
+                                            <td>
+                                                <select class="custom-select">
+                                                    <option selected>Open this select menu</option>
+                                                    <option value="1">One</option>
+                                                    <option value="2">Two</option>
+                                                    <option value="3">Three</option>
+                                                </select>
+                                            </td>
+                                            <td><input type="text" disabled value="1" /></td>
+                                            <td ><input type="text" disabled value="200 000$" /></td>
+                                            <td><input type="text" placeholder="Ghi chú" /></td>
+                                        </tr> 
+                                        
+                                    </tbody>
+                                </table>          
                             </div>
-                        </div>   
-                        <div className="bill-table">
-                            <table class="table table-sm">
-                                <thead className="text-center">
-                                    <tr>
-                                    <th scope="col">STT</th>
-                                    <th scope="col">Số Phòng</th>
-                                    <th scope="col">Số ngày thuê</th>
-                                    <th scope="col" >Đơn Giá</th>
-                                    <th scope="col">Ghi Chú</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <th scope="row" className="STT"></th>
-                                        <td>
-                                            <select class="custom-select">
-                                                <option selected>Open this select menu</option>
-                                                <option value="1">One</option>
-                                                <option value="2">Two</option>
-                                                <option value="3">Three</option>
-                                            </select>
-                                        </td>
-                                        <td><input type="text" disabled value="1" /></td>
-                                        <td ><input type="text" disabled value="200 000$" /></td>
-                                        <td><input type="text" placeholder="Ghi chú" /></td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row" className="STT"></th>
-                                        <td>
-                                            <select class="custom-select">
-                                                <option selected>Open this select menu</option>
-                                                <option value="1">One</option>
-                                                <option value="2">Two</option>
-                                                <option value="3">Three</option>
-                                            </select>
-                                        </td>
-                                        <td><input type="text" disabled value="1" /></td>
-                                        <td ><input type="text" disabled value="200 000$" /></td>
-                                        <td><input type="text" placeholder="Ghi chú" /></td>
-                                    </tr> <tr>
-                                        <th scope="row" className="STT"></th>
-                                        <td>
-                                            <select class="custom-select">
-                                                <option selected>Open this select menu</option>
-                                                <option value="1">One</option>
-                                                <option value="2">Two</option>
-                                                <option value="3">Three</option>
-                                            </select>
-                                        </td>
-                                        <td><input type="text" disabled value="1" /></td>
-                                        <td ><input type="text" disabled value="200 000$" /></td>
-                                        <td><input type="text" placeholder="Ghi chú" /></td>
-                                    </tr> 
-                                    
-                                </tbody>
-                            </table>          
+                            <div className="listroom-button text-center">
+                                <button type="button" class="btn btn-primary">Thanh toán</button>
+                                <button type="button" class="btn btn-success">Reset</button>
+                                <Link to='/' ><button type="button" class="btn btn-danger">Thoát</button></Link>
+                            </div>
                         </div>
-                        <div className="listroom-button text-center">
-                            <button type="button" class="btn btn-primary">Thanh toán</button>
-                            <button type="button" class="btn btn-success">Reset</button>
-                            <Link to='/' ><button type="button" class="btn btn-danger">Thoát</button></Link>
-                        </div>
-                    </div>
-                </form>    
+                    </form>    
                 </div>
-            </div>
             </div>
     </HomeScreen>
 }
