fcf1f745dfbc83534a48845ad97bcfa5eea188cf
liemnguyenthanh <liemdonduong@gmail.com>
2020-11-30 10:16:34 +0700

    Add frontend

---
+++ frontend/src/components/admin/report/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import HomeScreen from '../../../page/homeScreen';
+export default function ReportScreen(){
+    
+
+	return <HomeScreen>
+            ReportScreen
+    </HomeScreen>
+}
+
---
+++ frontend/src/components/admin/search/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import HomeScreen from '../../../page/homeScreen';
+export default function SearchScreen(){
+    
+
+	return <HomeScreen>
+            SearchScreen
+    </HomeScreen>
+}
+
