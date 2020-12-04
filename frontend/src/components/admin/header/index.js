af1bb7cdc6c9fb641143213dc39b8df489b818aa
A N H <51475160+anhamap1999@users.noreply.github.com>
2020-12-04 10:40:43 +0700

    logout

--- frontend/src/components/admin/header/index.js
+++ frontend/src/components/admin/header/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Header(){
- 
+    const onLogout = () => {
+        localStorage.removeItem('app-user', {});
+        localStorage.removeItem('token', '');
+    }
         return <div>
         <nav className="sb-topnav navbar navbar-expand navbar-light bg-clr">
             <a className="navbar-brand logo-brand" href="/">Quản lý Khách Sạn</a>
-            <Link to='/login' className="frnt-link"><i className="fas fa-user " />Logout</Link>  
+            <Link to='/login' className="frnt-link" onClick={onLogout}><i className="fas fa-user "/>Đăng xuất</Link>  
         </nav>
     </div>
 };
